803885160d28b4762cdabcd3127d9a36eb408617
chamikara12 <chamikaramadushanka55@gmail.com>
2025-11-15 14:11:40 +0530

    Unwant

--- gatepass-frontend/src/components/footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(props){
-    return(
-        <div className="bg-red-300 p-4 text-center">
-            <h3 >Phone:{props.number} </h3>
-            <h3 className="text-[]">© 2024 GatePass Management System. All rights reserved.</h3>
-        </div>
-    )
-}
--- gatepass-frontend/src/pages/home.jsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage(){
-    return(
-        <div className="w-full h-screen bg-yellow-200 flex justify-center items-center">
-            <h1 className="text-3xl font-bold underline">
-                Home Page
-            </h1>
-        </div>
-    )
-}
